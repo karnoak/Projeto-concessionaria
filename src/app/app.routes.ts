1ea3762d9d9c100848cfdc16c88829a06320b51e
Henry Mikael <dacksoulbr27@gmail.com>
2025-11-18 00:15:52 -0300

    primeiro commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Loja } from '../pages/loja/loja';
+import { Compra } from '../pages/compra/compra';
+import { Categoria } from '../pages/categoria/categoria';
+import { TestDrive } from '../pages/test-drive/test-drive';
+import { Home } from '../pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'loja', component: Loja },
+  { path: 'compra', component: Compra },
+  { path: 'categoria', component: Categoria },
+  { path: 'test-drive', component: TestDrive }
+];
